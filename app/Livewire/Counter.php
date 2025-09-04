82259fee2ced5b7fd8a55f63a0bfc0210d365cbd
Roni <ronielli.acicredi@sicoob.com.br>
2025-09-04 11:17:10 -0300

    criando um contador com livewire que adiciona um número toda vez que clica no botão, isso sem recarregar a página

---
+++ app/Livewire/Counter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $number = 0;
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+
+    public function increment()
+    {
+        $this->number++;
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <livewire:hello-world />
+    <livewire:counter />
 
     @livewireScripts
 </body>
